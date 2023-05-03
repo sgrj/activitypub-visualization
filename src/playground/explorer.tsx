bd9170cd4d12b9849adcaaf5a6d82f2b90ff54b1
Sebastian Jambor <seb.jambor@gmail.com>
2023-05-03 19:20:53 +0200

    extract explorer

---
+++ src/playground/explorer.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { ActivityPubExplorer } from '../index';
+
+import userEntity from './user-entity.json';
+
+import { useParams } from 'react-router-dom';
+
+export default function Explorer() {
+  const { data } = useParams();
+
+  const initialActivityJson = () => {
+    if (data == 'initial-data') {
+      return userEntity;
+    } else {
+      return null;
+    }
+  };
+
+  return (
+    <ActivityPubExplorer
+      fetchMethod={async (url) =>
+        fetch('http://localhost:3000/api/v1/json_ld?' + new URLSearchParams({ url }).toString())
+      }
+      initialActivityJson={initialActivityJson()}
+    />
+  );
+}
--- src/playground/index.tsx
+++ src/playground/index.tsx
@@ -1,44 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import { ActivityPubExplorer } from '../index';
-
-import Colors from './colors';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import '../input.css';
 import '../font.css';
 
-import userEntity from './user-entity.json';
-
-import { createBrowserRouter, RouterProvider, useParams } from 'react-router-dom';
 import Root from './root';
-
 import Log from './log';
+import Explorer from './explorer';
+import Colors from './colors';
 
 const elt = document.createElement('div');
 document.querySelector('body').appendChild(elt);
 document.querySelector('body').style.margin = '0';
-
-function Explorer() {
-  const { data } = useParams();
-
-  const initialActivityJson = () => {
-    if (data == 'initial-data') {
-      return userEntity;
-    } else {
-      return null;
-    }
-  };
-
-  return (
-    <ActivityPubExplorer
-      fetchMethod={async (url) =>
-        fetch('http://localhost:3000/api/v1/json_ld?' + new URLSearchParams({ url }).toString())
-      }
-      initialActivityJson={initialActivityJson()}
-    />
-  );
-}
 
 const router = createBrowserRouter([
   {
